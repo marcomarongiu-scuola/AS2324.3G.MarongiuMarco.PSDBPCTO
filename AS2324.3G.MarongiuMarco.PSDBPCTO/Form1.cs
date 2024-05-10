44ac54cbe17367a554b34337b90d7bba466e8949
marko marongiu <coentro07@gmail.com>
2024-05-10 09:09:29 +0200

    Aggiungere i file di progetto.

---
+++ AS2324.3G.MarongiuMarco.PSDBPCTO/Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2324._3G.MarongiuMarco.PSDBPCTO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
